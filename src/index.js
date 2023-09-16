30f3ed9fde02ae9db6bde200f60e20c52509afd7
Felix Laniyan <flaniyanjr@gmail.com>
2023-09-16 11:33:12 -0400

    task list and delete button made

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form= document.getElementById('create-task-form')
+
+  form.addEventListener('submit', (e) => {
+    /* When the button is clicked, we want to add the what the user input to the unordered list
+    */
+    e.preventDefault()
+    const userInput= e.target['new-task-description'].value
+    
+    const task= document.createElement('li')
+    const deleteButton= document.createElement('button')
+    deleteButton.innerText= 'X'
+    task.innerText= `${userInput}     `
+
+    document.getElementById('tasks').append(task)
+    task.append(deleteButton)
+
+    deleteButton.addEventListener('click', (e) => {
+      e.target.parentNode.remove()
+    })
+
+    e.target.reset()
+    
+  })
 });
